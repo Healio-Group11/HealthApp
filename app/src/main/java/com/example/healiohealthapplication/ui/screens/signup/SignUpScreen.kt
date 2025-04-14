1255fa4be4531b2320343dce357dda427107e706
Helmi Griffiths <t3grhe00@students.oamk.fi>
2025-04-14 18:33:39 +0300

    created signup screen string resources

--- app/src/main/java/com/example/healiohealthapplication/ui/screens/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/healiohealthapplication/ui/screens/signup/SignUpScreen.kt
@@ -16,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.healiohealthapplication.R
 import com.example.healiohealthapplication.navigation.Routes
 import com.example.healiohealthapplication.ui.components.BigButton
 import com.example.healiohealthapplication.ui.components.ChangePageText
@@ -29,40 +31,46 @@ import com.example.healiohealthapplication.ui.screens.shared.SharedViewModel
 fun SignUpScreen(navController: NavController, modifier: Modifier, viewModel: SignUpViewModel, sharedViewModel: SharedViewModel) {
     Scaffold { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding).fillMaxSize().padding(16.dp).background(MaterialTheme.colorScheme.background),
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Let's help you meet your goal",
+                text = stringResource(R.string.sign_up_screen_title),
                 style = MaterialTheme.typography.headlineSmall
             )
             Spacer(modifier = Modifier.height(34.dp))
             LoginAndSignUpOutlinedTextField(
                 value = viewModel.currentEmail,
                 onValueChange = { viewModel.currentEmail = it },
-                label = "Email",
+                label = stringResource(R.string.sign_up_screen_outlined_text_field_label_email),
                 keyboardType = KeyboardType.Email,
                 leadingIcon = Icons.Filled.Email,
-                iconContentDescription = "description",
+                iconContentDescription = stringResource(R.string.sign_up_screen_outlined_text_field_icon_desc_email),
             )
             LoginAndSignUpOutlinedTextField(
                 value = viewModel.currentPassword,
                 onValueChange = { viewModel.currentPassword = it },
-                label = "Password",
+                label = stringResource(R.string.sign_up_screen_outlined_text_field_label_password),
                 keyboardType = KeyboardType.Password,
                 isPassword = true,
                 leadingIcon = Icons.Filled.Lock,
-                iconContentDescription = "description",
+                iconContentDescription = stringResource(R.string.sign_up_screen_outlined_text_field_icon_desc_password),
             )
             Text(
-                text = "• Password must contain at least 6 characters",
+                text = stringResource(R.string.sign_up_screen_password_requirements_text),
                 style = MaterialTheme.typography.labelSmall,
-                modifier = Modifier.align(Alignment.Start).padding(top = 8.dp, start = 16.dp, end = 16.dp)
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(top = 8.dp, start = 16.dp, end = 16.dp)
             )
             Spacer(modifier = Modifier.height(34.dp))
             BigButton(
-                text = "Sign Up",
+                text = stringResource(R.string.sign_up_screen_submit_button_text),
                 onClick = {
                     viewModel.register(
                         navController,
@@ -75,8 +83,8 @@ fun SignUpScreen(navController: NavController, modifier: Modifier, viewModel: Si
             )
             Spacer(modifier = Modifier.height(20.dp))
             ChangePageText(
-                text = "Already have an account?",
-                linkText = "Log in"
+                text = stringResource(R.string.sign_up_screen_change_page_text),
+                linkText = stringResource(R.string.sign_up_screen_change_page_link_text)
             ) { navController.navigate(Routes.LOGIN) }
         }
     }
